b273b6057d48c882587026fb5bfd3946970b51e8
foozie3moons <matthew.bell.290@gmail.com>
2018-01-25 16:30:57 -0800

    updated ma clock, made it better yo!

--- clock.js
+++ clock.js
@@ -1,86 +1,26 @@
-var hour = document.querySelector("#hour");
-var second = document.querySelector("#second");
-var minute = document.querySelector("#minute");
+document.addEventListener("DOMContentLoaded", function() {
+  var redrawClock = function() {
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
 
-var hourDegree = 0;
-var secondDegree = 0;
-var minuteDegree = 0;
+    var secondDegree = second * 360 / 60;
+    var minuteDegree = (minute * 360 / 60) + (secondDegree / 60);
+    var hourDegree = (hour * 360 / 12) + (minuteDegree / 60);
 
-function rotate(element, degrees) {
-  element.style.transform = "rotate(" + degrees + "deg)";
-}
+    document.getElementById('year').textContent = moment(date).format('YYYY');
+    document.getElementById('month').textContent = moment(date).format('MMM');
+    document.getElementById('day').textContent = moment(date).format('DD');
+    document.getElementById('hour').style.transform = "rotate(" + hourDegree + "deg)";;
+    document.getElementById('minute').style.transform = "rotate(" + minuteDegree + "deg)";;
+    document.getElementById('second').style.transform = "rotate(" + secondDegree + "deg)";;
+  }
 
-function rotateSecond() {
-  secondDegree = secondDegree + 6;
-  second.style.transform = "rotate(" + secondDegree + "deg)";
-}
+  redrawClock();
 
-function rotateMinute() {
-  minuteDegree = minuteDegree + 6;
-  minute.style.transform = "rotate(" + minuteDegree + "deg)";
-}
+  var timeout = setInterval(function() {
+    redrawClock();
+  }, 1000);
 
-function rotateHour() {
-  hourDegree = hourDegree + 6;
-  hour.style.transform = "rotate(" + hourDegree + "deg)";
-}
-
-setInterval(function() {
-  rotateSecond();
-}, 1000);
-
-// I don't know why this rotates before 60 seconds are up at the start
-// but after about 4 minutes it is right on track
-setInterval(function() {
-  rotateMinute();
-}, 60000);
-setInterval(function() {
-  rotateHour();
-}, 60000 * 12);
-
-
-// attempt at using the Date object
-/*
-
-var hour = document.querySelector("#hour");
-var second = document.querySelector("#second");
-var minute = document.querySelector("#minute");
-
-var now = new Date();
-
-var hourDegree = now.getHours() * 6;
-var secondDegree = now.getSeconds() * 6;
-var minuteDegree = now.getMinutes() * 6;
-
-console.log(typeof hourDegree);
-
-console.log(hour);
-
-function rotateSecond() {
-  secondDegree = secondDegree + 6;
-  second.style.transform = "rotate(" + secondDegree + "deg)";
-}
-
-function rotateMinute() {
-  minuteDegree = minuteDegree + 6;
-  minute.style.transform = "rotate(" + minuteDegree + "deg)";
-}
-
-function rotateHour() {
-  hourDegree = hourDegree + 6;
-  hour.style.transform = "rotate(" + hourDegree + "deg)";
-}
-
-setInterval(function() {
-  rotateSecond();
-}, 1000);
-// I don't know why this rotates before 60 seconds are up at the start
-// but after about 4 minutes it is right on track
-setInterval(function() {
-  rotateMinute();
-}, 60000);
-setInterval(function() {
-  rotateHour();
-}, 60000 * 12);
-
-*/
+});
